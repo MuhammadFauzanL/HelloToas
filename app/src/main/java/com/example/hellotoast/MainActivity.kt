1f08773db9f0e6a177e78ee1b389998ba57a5ab7
M Fauzan Lubada <fauzanlubada5@gmail.com>
2025-10-01 23:52:26 +0700

    first

--- app/src/main/java/com/example/hellotoast/MainActivity.kt
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.example.hellotoast
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     private var mCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val mShowCount = findViewById<TextView>(R.id.show_count)
         val buttonCountUp = findViewById<Button>(R.id.button_count)
         val buttonToast = findViewById<Button>(R.id.button_toast)
-
+        val buttonSwitchPage = findViewById<Button>(R.id.button_switchpage)
         buttonCountUp.setOnClickListener {
             mCount++
             mShowCount.text = mCount.toString()
@@ -24,7 +25,12 @@ class MainActivity : AppCompatActivity() {
 
         buttonToast.setOnClickListener {
             val tulisan = mShowCount.text.toString()
-            Toast.makeText(this, "Angka yang dimunculkan $tulisan", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Angka yang ditampilkan: $tulisan", Toast.LENGTH_LONG).show()
+        }
+
+        buttonSwitchPage.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
         }
     }
 }
